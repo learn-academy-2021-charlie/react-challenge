6eabc6372ad81cb60c745765575d98ea1170ef18
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-07-20 14:51:19 -0700

    started dice roll counter

---
+++ dice-roller-todd-alfonso/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Rolls from './components/Rolls.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dice Roller</h1>
+      <h2>Rolls some dice</h2>
+      <Rolls/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ dice-roller-todd-alfonso/src/components/Rolls.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+class Rolls extends Component{
+    render(){
+        return(
+            <h1>This is where we'll count dice rolls</h1>
+        )
+    }
+
+}
+
+export default Rolls
