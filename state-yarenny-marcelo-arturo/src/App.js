ca0bb087e2f20138758fd841a87d5d4ad98443e4
byarenny <byarenny@gmail.com>
2021-07-20 11:57:13 -0700

    finished part of challenge

---
+++ state-yarenny-marcelo-arturo/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+// The main css file is called App.css, it is imported to App.js here
+import './App.css'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        backgroundColor : ["green", "blue", "yellow", "red", "purple", "orange"],
+        currentColor: "green"
+
+    }
+  }
+   colorChange = () => { 
+    //  const {backgroundColor} = this.state
+    // this.setState({currectColor: this.state.backgroundColor})
+    this.setState({currentColor: this.state.backgroundColor[Math.floor(Math.random() * 5)]})
+  }
+
+    
+  render() {
+    return (
+      <>
+        <h1>Colored square</h1>
+        <button onClick={this.colorChange} style={{backgroundColor: this.state.currentColor}}> Press me!</button>
+        <p>"green"</p>
+
+      </>
+    )
+  }
+}
+
+export default App;
+
