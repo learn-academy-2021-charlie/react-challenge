d8025663206d6245514754e62aa95cdc1b25b022
Andy19936 <andy19936@gmail.com>
2021-07-22 15:55:51 -0700

    initial switch

---
+++ functional-props-andy-todd/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import react,{Component} from 'react'
+import './component/Menuitems'
+import MenuItems from './component/Menuitems';
+
+class App extends Component{
+  constructor(props){
+  super (props)  
+  this.state = {
+    cart: [],
+    menuItems: ["Cheeseburger $4.50","Fries $1.50","Hot Dog $2.00"]
+  }}
+ 
+addItem=(item =>{
+alert(item)
+})
+
+
+render(){
+  return (
+  <>
+  <h1> Cheesey Joe's </h1>
+
+  < MenuItems onClick />
+ 
+  </>
+  )
+  }
+}
+  
+
+export default App;
---
+++ functional-props-andy-todd/src/component/Menuitems.js
@@ -0,0 +1,19 @@
+ import React, { Component } from 'react'
+ 
+
+ class MenuItems extends Component {
+  render() {
+    return (
+      <>
+          <ul>
+            
+            <li>Cheeseburger $4.50</li>
+            <li>Fries $1.50</li>
+            <li>Hot Dog $2.00</li>
+          </ul>
+      </>
+    )
+  }
+}
+
+export default MenuItems
