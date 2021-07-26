6c2767e0c4d7f3ec8b2d25ee7c9d31dbed9680ae
christophermackey <c.mackey@me.com>
2021-07-26 12:02:11 -0700

    initial commit

---
+++ robot/src/components/GoodRobot.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class GoodRobot extends Component {
+    render(){
+        return(
+            <>
+                <h4>GoodRobot</h4>
+                
+            </>
+        )
+    }
+}
+
+export default GoodRobot
---
+++ robot/src/components/KanyeBot5000.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class KanyeBot5000 extends Component {
+    render(){
+        return(
+            <>
+                <h4>KanyeBot5000</h4>
+            </>
+        )
+    }
+}
+export default KanyeBot5000
